6afabcf58b3c5a17e6ff654c4f6447025848952e
Thorsten <t.kisner@web.de>
2015-07-29 10:36:20 +0100

    prefix mapper added

---
+++ xml/src/main/java/de/kisner/xbtjl/model/xml/JbtlNsPrefixMapper.java
@@ -0,0 +1,24 @@
+package de.kisner.xbtjl.model.xml;
+
+import net.sf.exlp.xml.ns.NsPrefixMapperInterface;
+
+import com.sun.xml.bind.marshaller.NamespacePrefixMapper;
+
+public class JbtlNsPrefixMapper extends NamespacePrefixMapper implements NsPrefixMapperInterface
+{
+    public String getPreferredPrefix(String namespaceUri, String suggestion, boolean requirePrefix)
+    {
+        if("http://jbtl.sf.net".equals(namespaceUri) ){return "jbtl";}
+        if("http://jbtl.sf.net/bittorrent".equals(namespaceUri) ){return "bt";}
+       
+        return suggestion;
+    }
+
+    public String[] getPreDeclaredNamespaceUris()
+    {
+    	String[] result = new String[3];
+
+    	result = new String[0];
+        return result;
+    }
+}
